08046f40dff9b16513b5dc695c1af790b5a6fa7a
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-04-19 09:28:20 +0530

    add private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Home from "./pages/HomePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
